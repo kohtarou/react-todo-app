08bc8031e63322501d65dca1e6157ecbae1ce205
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-11-17 12:13:57 +0900

    cofyu

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,3 +1,4 @@
+//import React from "react";
 import { Todo } from "./types";
 import dayjs from "dayjs";
 
@@ -5,6 +6,8 @@ type Props = {
   todo: Todo;
   updateIsDone: (id: string, value: boolean) => void;
   remove: (id: string) => void;
+  priority: number;
+  deadline: Date | null;
 };
 
 const TodoItem = (props: Props) => {
@@ -18,6 +21,16 @@ const TodoItem = (props: Props) => {
           checked={todo.isDone}
           onChange={(e) => props.updateIsDone(todo.id, e.target.checked)}
           className="mr-2 cursor-pointer"
+          style={{
+            width: "1.25rem", // サイズを調整
+            height: "1.25rem", // サイズを調整
+            borderRadius: "50%", // 丸くする
+            border: "2px solid #ccc", // 枠線の色を調整
+            appearance: "none", // デフォルトのスタイルを無効化
+            WebkitAppearance: "none", // デフォルトのスタイルを無効化
+            MozAppearance: "none", // デフォルトのスタイルを無効化
+            cursor: "pointer", // カーソルをポインターに変更
+          }}
         />
         <label className="mr-4">{todo.name}</label>
       </div>
