1e2d8ae6ab908313c6ae4d342ec64e676ff69163
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-11-17 11:45:55 +0900

    aer

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,4 +1,3 @@
-//import React from "react";
 import { Todo } from "./types";
 import dayjs from "dayjs";
 
@@ -6,43 +5,36 @@ type Props = {
   todo: Todo;
   updateIsDone: (id: string, value: boolean) => void;
   remove: (id: string) => void;
-  priority: number;
-  deadline: Date | null;
 };
 
 const TodoItem = (props: Props) => {
   const todo = props.todo;
+
   return (
-    <div className="flex items-center ">
+    <div className="flex justify-between items-start p-2 border-b">
       <div className="flex items-center">
         <input
           type="checkbox"
           checked={todo.isDone}
           onChange={(e) => props.updateIsDone(todo.id, e.target.checked)}
-          className="mr-1.5 cursor-pointer rounded-full"
+          className="mr-2 cursor-pointer"
         />
+        <label className="mr-4">{todo.name}</label>
       </div>
 
-      <div className="flex items-center">
-        <label>{todo.name}</label>
-      </div>
-
-      <div className="flex-col flex-center">
-        <div class-Nam="ml-suto">優先度:{todo.priority}</div>
-
-        <div>
-          {todo.deadline && (
-            <div className="ml-auto">
-              期限: {dayjs(todo.deadline).format("YYYY年M月D日 H時m分")}
-            </div>
-          )}
-        </div>
+      <div className="flex flex-col items-center mx-4">
+        <div className="text-sm">優先度: {todo.priority}</div>
+        {todo.deadline && (
+          <div className="text-sm">
+            期限: {dayjs(todo.deadline).format("YYYY年M月D日 H時m分")}
+          </div>
+        )}
       </div>
 
       <div>
         <button
           onClick={() => props.remove(todo.id)}
-          className="flex flex-row space-x-2 item-center rounded-md bg-slate-200 px-2 py-1 text-sm font-bold text-white hover:bg-red-500"
+          className="rounded-md bg-slate-200 px-2 py-1 text-sm font-bold text-white hover:bg-red-500"
         >
           削除
         </button>
