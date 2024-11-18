14493d40cafa1f236e658486bc23d2ac934f216e
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-11-18 16:57:16 +0900

    etsrdtyfg

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -3,7 +3,7 @@ import { Todo } from "./types";
 import dayjs from "dayjs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // 追加
 import {
-  faFile,
+  faBalanceScale,
   faClock,
   faCircleXmark,
 } from "@fortawesome/free-solid-svg-icons"; // 追加
@@ -21,19 +21,18 @@ const TodoItem = (props: Props) => {
   const todo = props.todo;
 
   return (
-    <div className="flex justify-between items-center p-4 border-2 border-[#afafaf] bg-[#E6E6E6] rounded-[20px]">
+    <div className="flex justify-between items-center p-4 border-1 border-[#afafaf] rounded-[20px] shadow-custom">
       <div className="flex items-center">
         <input
           type="checkbox"
           checked={todo.isDone}
           onChange={(e) => props.updateIsDone(todo.id, e.target.checked)}
-          className="mr-2 cursor-pointer w-5 h-5 rounded-full border-2 border-gray-400 bg-[#f0f0f0] appearance-none checked:bg-blue-500" // Tailwind CSS クラスを使用
+          className="mr-2 cursor-pointer w-5 h-5 rounded-full border-1 border-gray-400  appearance-none checked:bg-blue-500" // Tailwind CSS クラスを使用
         />
-        <FontAwesomeIcon icon={faFile} className="mr-2" />{" "}
-        {/* アイコンを追加 */}
-        <label className="mr-4">{todo.name}</label>
+        <label className="mr-4 font-bold">{todo.name}</label>
       </div>
       <div className="flex flex-col items-start mx-4">
+        <FontAwesomeIcon icon={faBalanceScale} />
         <div className="text-sm">優先度: {todo.priority}</div>
         {todo.deadline && (
           <div className="text-sm">
@@ -46,7 +45,7 @@ const TodoItem = (props: Props) => {
       <div>
         <button
           onClick={() => props.remove(todo.id)}
-          className="rounded-full bg-red-500 p-2 text-white hover:bg-red-600" // ボタンのスタイルを変更
+          //className="rounded-full bg-red-500 p-2 text-white hover:bg-red-600"  ボタンのスタイルを変更
         >
           <FontAwesomeIcon icon={faCircleXmark} /> {/* アイコンを追加 */}
         </button>
