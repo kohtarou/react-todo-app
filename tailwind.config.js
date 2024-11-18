c630c0e91c1a4d8acb1bf04cd0970e07f4726a8c
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-11-18 17:15:43 +0900

    tghj

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      boxShadow: {
+        custom: "3px 3px 10px 0px #6e6e6e", // カスタムシャドウを追加
+      },
     },
-    plugins: [],
+  },
+  variants: {},
+  plugins: [],
 };
